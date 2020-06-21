ace2e9176bc82a4a0a9ba70f692224435ec5e418
yoshihitoh <yoshihito.arih@gmail.com>
2020-06-21 17:18:13 +0900

    Change lambda app, access to AWS S3 via Rusoto.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
 use std::error::Error as StdError;
 
 use lambda_runtime::{error::HandlerError, lambda, Context};
+use rusoto_core::Region;
+use rusoto_s3::{S3Client, S3};
 use serde::{Deserialize, Serialize};
+use tokio::runtime::Runtime;
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct CustomEvent {
-    first_name: String,
-}
+struct CustomEvent {}
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
 struct CustomOutput {
-    message: String,
+    bucket_names: Vec<String>,
+}
+
+struct App {
+    s3_client: S3Client,
+    runtime: Runtime,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        let s3_client = S3Client::new(Region::default());
+        let runtime = Runtime::new().unwrap();
+        App { s3_client, runtime }
+    }
 }
 
 fn main() -> Result<(), Box<dyn StdError>> {
@@ -20,8 +34,26 @@ fn main() -> Result<(), Box<dyn StdError>> {
     Ok(())
 }
 
-fn handler(event: CustomEvent, _context: Context) -> Result<CustomOutput, HandlerError> {
-    Ok(CustomOutput {
-        message: format!("Hello, {}!", event.first_name),
-    })
+fn handler(_event: CustomEvent, _context: Context) -> Result<CustomOutput, HandlerError> {
+    let mut app = App::default();
+    let bucket_names = app
+        .runtime
+        .block_on(bucket_names(&app.s3_client))
+        .map_err(|e| HandlerError::from(format!("error: {:?}", e).as_str()))?;
+    Ok(CustomOutput { bucket_names })
+}
+
+async fn bucket_names(s3_client: &S3Client) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let output = s3_client.list_buckets().await?;
+    let names = output
+        .buckets
+        .map(|bs| {
+            bs.into_iter()
+                .filter(|b| b.name.is_some())
+                .map(|b| b.name.unwrap())
+                .collect()
+        })
+        .unwrap_or_else(Vec::new);
+
+    Ok(names)
 }
