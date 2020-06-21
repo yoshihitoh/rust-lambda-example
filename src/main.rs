93309fc45d4e9fe5e6cdf889acfad8fbf7524772
yoshihitoh <yoshihito.arih@gmail.com>
2020-06-21 17:32:41 +0900

    Fix performance problem on handler function.

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,94 @@
 use std::error::Error as StdError;
+use std::io;
 
 use lambda_runtime::{error::HandlerError, lambda, Context};
-use rusoto_core::Region;
-use rusoto_s3::{S3Client, S3};
+use rusoto_core::{Region, RusotoError};
+use rusoto_s3::{GetObjectError, GetObjectOutput, GetObjectRequest, S3Client, StreamingBody, S3};
 use serde::{Deserialize, Serialize};
+use tokio::io::AsyncReadExt;
 use tokio::runtime::Runtime;
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct CustomEvent {}
+struct CustomEvent {
+    bucket_name: String,
+    object_key: String,
+}
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
-struct CustomOutput {
-    bucket_names: Vec<String>,
-}
+struct CustomOutput {}
 
 struct App {
     s3_client: S3Client,
-    runtime: Runtime,
 }
 
 impl Default for App {
     fn default() -> Self {
         let s3_client = S3Client::new(Region::default());
-        let runtime = Runtime::new().unwrap();
-        App { s3_client, runtime }
+        App { s3_client }
     }
 }
 
 fn main() -> Result<(), Box<dyn StdError>> {
-    lambda!(handler);
+    let app = App::default();
+    let mut runtime = Runtime::new().expect("Cannot create a tokio runtime.");
+
+    lambda!(|event, context| handler(&mut runtime, &app, event, context));
+
     Ok(())
 }
 
-fn handler(_event: CustomEvent, _context: Context) -> Result<CustomOutput, HandlerError> {
-    let mut app = App::default();
-    let bucket_names = app
-        .runtime
-        .block_on(bucket_names(&app.s3_client))
+fn handler(
+    runtime: &mut Runtime,
+    app: &App,
+    event: CustomEvent,
+    _context: Context,
+) -> Result<CustomOutput, HandlerError> {
+    runtime
+        .block_on(handle_s3_object(&app, event))
         .map_err(|e| HandlerError::from(format!("error: {:?}", e).as_str()))?;
-    Ok(CustomOutput { bucket_names })
+
+    Ok(CustomOutput {})
 }
 
-async fn bucket_names(s3_client: &S3Client) -> Result<Vec<String>, Box<dyn std::error::Error>> {
-    let output = s3_client.list_buckets().await?;
-    let names = output
-        .buckets
-        .map(|bs| {
-            bs.into_iter()
-                .filter(|b| b.name.is_some())
-                .map(|b| b.name.unwrap())
-                .collect()
-        })
-        .unwrap_or_else(Vec::new);
+async fn handle_s3_object(
+    app: &App,
+    event: CustomEvent,
+) -> Result<(), Box<dyn StdError>> {
+    let out = get_object(&app.s3_client, event.bucket_name, event.object_key)
+        .await?;
 
-    Ok(names)
+    if let (Some(body), Some(length)) = (out.body, out.content_length) {
+        let data = read_body(body, length as usize).await?;
+        // オブジェクトのデータを使って何かする
+    }
+
+    Ok(())
+}
+
+async fn get_object(
+    s3_client: &S3Client,
+    bucket_name: String,
+    object_key: String,
+) -> Result<GetObjectOutput, RusotoError<GetObjectError>> {
+    let input = GetObjectRequest {
+        bucket: bucket_name,
+        key: object_key,
+        ..Default::default()
+    };
+    let output = s3_client.get_object(input).await?;
+    Ok(output)
+}
+
+async fn read_body(
+    body: StreamingBody,
+    content_length: usize,
+) -> Result<Vec<u8>, io::Error> {
+    let mut reader = body.into_async_read();
+
+    let mut data = Vec::with_capacity(content_length);
+    reader.read_to_end(&mut data).await?;
+
+    Ok(data)
 }
